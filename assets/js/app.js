4d2f4cc9d235084a75adb83585f7393de2a0a586
Daniel Iñiguez <danielig9@outlook.com>
2023-11-27 14:36:31 -0600

    se agregan cambios intentando hacer funcionar el codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,32 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name'); //faltaba el "." para referenciar la clase correctamente
+const $b = document.querySelector('.blog'); //Tenia un # cuando tenia que ser "." ya que es una clase y no un id
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+//Falta la palabra "async" en la función para poder usar el await
+async function displayUser(usersEndpoint) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //Se intento agregar un try catch para las exepciones
+  try {
+    const response = await fetch(usersEndpoint); //Se pasa directamente el link 
+    const data = response.json(); //Se agrego esta linea para cambiar el formato json
+    console.log(data);
+    //Las comillas de las siguientes 3 lineas estan mal deben ser las invertidas (``)
+    $n.textContent = `${data.name}`;
+    $b.textContent = `${data.blog}`;
+    $l.textContent = `${data.location}`;
+  } catch (error) {
+    console.log('OH NO!');
+    console.log(error);
+    $n.textContent = `Algo salió mal: ${error}`
+  }
 }
 
-function handleError(err) {
+/* function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
-}
+  $n.textContent = `Algo salió mal: ${err}`
+} */
 
-displayUser('stolinski').catch(handleError);
+displayUser(baseEndpoint); //se cambio el llamado de la funcion
